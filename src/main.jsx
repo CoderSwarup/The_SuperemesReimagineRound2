dcd5ea8048e78f025f28f3e32326aa96713a36d9
Suryansh Singh <supremesofficialyt@gmail.com>
2024-07-19 01:18:00 +0530

    Files Updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,11 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+<<<<<<< HEAD
     <App />
+=======
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+>>>>>>> da1a99796bcd011d2fdfcd5d3d1263c741956c8c
 )
